616535835afcb376412b85314015e97e60c79e79
clement lemaire <c.lemaire027@gmail.com>
2020-05-23 16:31:40 +0200

    crud almost ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
 end
